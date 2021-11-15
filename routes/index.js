914eb48b69346731b80ed3888f72f3b479d3205e
Ramya M M <Ramya.M.M@gds.ey.com>
2021-11-15 20:57:16 +0530

    apis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const Router = require("express").Router;
 
 const masterData = require('./masterdata')
+const video = require('./video')
 
 const routes = Router()
 
 routes.use('/masterData', masterData)
+routes.use('/video', video)
 
 module.exports = routes
