8f73220bda81c63946c04cfbf16222b1fb044558
souvikbajrangi <souvikmajumder31@gmail.com>
2021-11-17 15:38:38 +0530

    fix 4

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const routes = require("./routes");
 const port = process.env.PORT || 8001;
 const COS = require("ibm-cos-sdk");
 const WebAppStrategy = require("ibmcloud-appid").WebAppStrategy;
+const APIStrategy = require("ibmcloud-appid").APIStrategy;
 const CALLBACK_URL = "/ibm/cloud/appid/callback";
 const helmet = require("helmet")
 
@@ -30,7 +31,7 @@ if (process.env.ENVIRONMENT != "LOCAL") {
     //     })
     // );
 
-    // app.use(passport.initialize());
+    app.use(passport.initialize());
     // app.use(passport.session());
     // app.use(passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
 
@@ -38,18 +39,22 @@ if (process.env.ENVIRONMENT != "LOCAL") {
 
     // }));
 
-    passport.use(
-        new WebAppStrategy({
-            tenantId: "44516919-34fa-4e47-a1de-9e0462e11af4",
-            clientId: "4aff0bc4-2465-4564-af0c-dc37cd9833eb",
-            secret: "Nzk1MjYyNmEtOGNiNy00NzMxLTllMWYtYTIxYTE3YTllODQw",
-            oauthServerUrl:
-                "https://eu-gb.appid.cloud.ibm.com/oauth/v4/44516919-34fa-4e47-a1de-9e0462e11af4",
-            redirectUri:
-                "http://raor-node-dev-raor.workshop-team-five-bb0dafd08526894d1a8ae848e8bd8099-0000.eu-gb.containers.appdomain.cloud" +
-                CALLBACK_URL,
-        })
-    );
+    // passport.use(
+    //     new WebAppStrategy({
+    //         tenantId: "44516919-34fa-4e47-a1de-9e0462e11af4",
+    //         clientId: "4aff0bc4-2465-4564-af0c-dc37cd9833eb",
+    //         secret: "Nzk1MjYyNmEtOGNiNy00NzMxLTllMWYtYTIxYTE3YTllODQw",
+    //         oauthServerUrl:
+    //             "https://eu-gb.appid.cloud.ibm.com/oauth/v4/44516919-34fa-4e47-a1de-9e0462e11af4",
+    //         redirectUri:
+    //             "http://raor-node-dev-raor.workshop-team-five-bb0dafd08526894d1a8ae848e8bd8099-0000.eu-gb.containers.appdomain.cloud" +
+    //             CALLBACK_URL,
+    //     })
+    // );
+
+    passport.use(new APIStrategy({
+        oauthServerUrl: "https://eu-gb.appid.cloud.ibm.com/oauth/v4/44516919-34fa-4e47-a1de-9e0462e11af4"
+    }));
 
     // passport.serializeUser(function (user, cb) {
     //     cb(null, user);
@@ -59,9 +64,7 @@ if (process.env.ENVIRONMENT != "LOCAL") {
     //     cb(null, obj);
     // });
 
-    app.get("/auth/login", passport.authenticate(WebAppStrategy.STRATEGY_NAME, { successRedirect: SUCCESS_REDIRECT_URL, forceLogin: true }));
-
-    app.get(CALLBACK_URL, passport.authenticate(WebAppStrategy.STRATEGY_NAME), { allowAnonymousLogin: true });
+    
 
     // app.get(
     //     "/protected_resource",
@@ -79,7 +82,9 @@ if (process.env.ENVIRONMENT != "LOCAL") {
     //     }
     // );
 
-    app.use("/api", passport.authenticate(WebAppStrategy.STRATEGY_NAME), routes);
+    // app.use("/api", passport.authenticate(WebAppStrategy.STRATEGY_NAME), routes);
+    app.use("/api", passport.authenticate(APIStrategy.STRATEGY_NAME, { session: false}), routes);
+    
 
 } else {
 
